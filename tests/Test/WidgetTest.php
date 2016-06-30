706983eb0933ba2397a3c22875966d4ee9313bc0
Sven Baumann <baumann.sv@gmail.com>
2016-06-30 09:28:28 +0200

    add php unit test

---
+++ tests/Test/WidgetTest.php
@@ -0,0 +1,29 @@
+<?php
+
+/**
+ * Avisota newsletter and mailing system
+ * Copyright © 2016 Sven Baumann
+ *
+ * PHP version 5
+ *
+ * @copyright  way.vision 2016
+ * @author     Sven Baumann <baumann.sv@gmail.com>
+ * @package    avisota/contao-selectri
+ * @license    LGPL-3.0+
+ * @filesource
+ */
+
+namespace Avisota\Test\Contao\Selectri;
+
+/**
+ * Class WidgetTest
+ *
+ * @package Avisota\Test\Contao\Selectri
+ */
+class WidgetTest extends \PHPUnit_Framework_TestCase
+{
+
+    public function testGenerate()
+    {
+    }
+}
